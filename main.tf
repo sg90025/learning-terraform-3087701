da8624e2dfb1313987a5548221621e8481fdb030
sg90025 <sudhir.gogada@gmail.com>
2024-09-05 20:19:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,8 +53,8 @@ resource "aws_security_group" "blog" {
 
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
-  from_port   = "80"
-  to_port     = "80"
+  from_port   = 80
+  to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -63,8 +63,8 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
-  from_port   = "443"
-  to_port     = "443"
+  from_port   = 443
+  to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
@@ -73,8 +73,8 @@ resource "aws_security_group_rule" "blog_https_in" {
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type         = "egress"
-  from_port    = "0"
-  to_port      = "0"
+  from_port    = 0
+  to_port      = 0
   protocol     = "-1"
   cidr_blocks  = ["0.0.0.0/0"]
 
