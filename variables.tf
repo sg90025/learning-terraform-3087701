1cf0ef1e873bc306c10e10181e20a423a918f72a
sg90025 <sudhir.gogada@gmail.com>
2024-09-06 22:35:14 -0700

    curly

--- variables.tf
+++ variables.tf
@@ -33,21 +33,6 @@ variable "environment" {
     network_prefix = "10.0"
   }
 
-
-}
-
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
 }
 
 
